b8dbc45f4427b70f249a1d524473ef07e5ddd935
Kevin Chiwakaya <kchiwakaya@gmail.com>
2023-06-03 17:42:07 +0200

    make model

--- farmersapp/admin.py
+++ farmersapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Farmer
+from .models import Farm, Farmer, Venture
 # Register your models here.
 
 admin.site.register(Farmer)
+admin.site.register(Farm)
+admin.site.register(Venture)
--- farmersapp/models.py
+++ farmersapp/models.py
@@ -14,3 +14,27 @@ class Farmer(models.Model):
 
     def __str__(self):
      return self.name +" "+ self.surname
+
+
+class Farm(models.Model):
+    farmer = models.ForeignKey(Farmer,on_delete = models.CASCADE)
+    farm_name = models.CharField(max_length=500)
+    plot_number = models.CharField(max_length=250,null=True, blank=True)
+    extend = models.DecimalField(max_digits= 10,decimal_places= 4)
+    district = models.CharField(max_length=20)
+    province = models.CharField(max_length=250)
+    ward = models.CharField(max_length=10,null=True, blank=True)
+    tenure_type = models.CharField(max_length=50)
+    water_availability  = models.CharField(max_length=250)
+    id = models.UUIDField(default=uuid.uuid1,unique=True,primary_key=True,editable=False)
+
+    def __str__(self):
+     return self.farm_name
+
+class Venture(models.Model):
+   farm = models.ForeignKey(Farm,on_delete = models.CASCADE)
+   descripiton = models.TextField()
+   supporting_images = models.CharField(max_length=250,null = True, blank=True)
+   amount = models.DecimalField(max_digits= 20,decimal_places= 4,null = True,blank = True)
+   id = models.UUIDField(default=uuid.uuid1,unique=True,primary_key=True,editable=False)
+
