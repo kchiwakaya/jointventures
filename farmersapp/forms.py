ba8ff2e74b510c081c3048054407764e0f348e7c
Kevin Chiwakaya <kchiwakaya@gmail.com>
2023-06-06 13:49:05 +0200

    venture form

--- farmersapp/forms.py
+++ farmersapp/forms.py
@@ -1,17 +1,25 @@
-from django.forms import ModelForm
+from django import  forms
 from .models import Farmer, Farm, Venture
 
-class FarmerForm(ModelForm):
+class FarmerForm(forms.ModelForm):
     class Meta:
         model = Farmer
         fields = '__all__'
 
-class FarmForm(ModelForm):
+class FarmForm(forms.ModelForm):
     class Meta:
         model = Farm
         fields = '__all__'
 
-class VentureForm(ModelForm):
+class VentureForm(forms.ModelForm):
     class Meta:
         model = Venture
-        fields = '__all__'
+        fields = ('farm','descripiton','supporting_images','amount')
+
+        widgets = {
+            'farm':forms.TextInput(attrs={'class':'form-control'}),
+            'descripiton':forms.Textarea(attrs={'class':'form-control'}),
+            'supporting_images':forms.TextInput(attrs={'class':'form-control'}),
+            'amount':forms.TextInput(attrs={'class':'form-control'}),
+
+        }
