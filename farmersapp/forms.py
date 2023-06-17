b37f2db4f4c158a9777a1c4aca163af6472d24d7
Kevin Chiwakaya <kchiwakaya@gmail.com>
2023-06-17 20:26:19 +0200

    zvafamba

--- farmersapp/forms.py
+++ farmersapp/forms.py
@@ -12,35 +12,39 @@ class FarmerForm(forms.ModelForm):
             'surname':forms.TextInput(attrs={'class':'form-control'}),
             'phone':forms.TextInput(attrs={'class':'form-control'}),
             'address':forms.TextInput(attrs={'class':'form-control'}),
-            'gender':forms.TextInput(attrs={'class':'form-control'}),
+            'gender':forms.Select(attrs={'class':'form-control'}),
             'national_id':forms.TextInput(attrs={'class':'form-control'})
         }
+
 
 class FarmForm(forms.ModelForm):
     class Meta:
         model = Farm
         fields = ('farm_name','plot_number','extend','district',
                   'province','ward','tenure_type','irrigation','water_availability')
+       
         widgets = {
             'farm_name':forms.TextInput(attrs={'class':'form-control'}),
             'plot_number':forms.TextInput(attrs={'class':'form-control'}),
             'extend':forms.TextInput(attrs={'class':'form-control'}),
             'district':forms.TextInput(attrs={'class':'form-control'}),
-            'province':forms.TextInput(attrs={'class':'form-control'}),
+            'province':forms.Select(attrs={'class':'form-control'}),
             'ward':forms.TextInput(attrs={'class':'form-control'}),
-            'tenure_type':forms.TextInput(attrs={'class':'form-control'}),
-            'irrigation':forms.TextInput(attrs={'class':'form-control'}),
-            'water_availability': forms.TextInput(attrs={'class':'form-control'}),
+            'tenure_type':forms.Select(attrs={'class':'form-control'}),
+            'irrigation':forms.Select(attrs={'class':'form-control'}),
+            'water_availability': forms.Select(attrs={'class':'form-control'}),
 
         }
+      
 class VentureForm(forms.ModelForm):
     class Meta:
         model = Venture
-        fields = ('farm','descripiton','supporting_images','amount')
-
+        fields = ('descripiton','supporting_images','amount')
+    
         widgets = {
             'descripiton':forms.Textarea(attrs={'class':'form-control'}),
             'supporting_images':forms.TextInput(attrs={'class':'form-control'}),
             'amount':forms.TextInput(attrs={'class':'form-control'}),
 
         }
+       
--- farmersapp/models.py
+++ farmersapp/models.py
@@ -2,29 +2,12 @@ from django.db import models
 import uuid
 
 # Create your models here.
-tenure = (
-   ('Off','Offer Letter'),
-   ('99','99-Year Lease'),
-   ('A1','A1 Permit'),
-   ('AP', 'A1 Temporal Permit'),
-   ('A2','A2 Permit'),
-   ('Ttl','Title Deed'),
-   ('DG','Deed of Grant'),
-   ('CCT','Certificate of Consolidated Title'),
-)
-gender = (
+
+class Farmer(models.Model):
+    gender = (
    ('M','Male'),
    ('F','Female'),
-)
-water = (
-   ('D','Dam'),
-   ('R','River'),
-   ('B','Borehole'),
-   ('W', 'Weir'),
-   ('Wl','Well'),
-   ('N','None'),
-)
-class Farmer(models.Model):
+   )
     name = models.CharField(max_length=250)
     othername = models.CharField(max_length=250,null=True, blank=True)
     surname = models.CharField(max_length=250)
@@ -36,7 +19,27 @@ class Farmer(models.Model):
     def __str__(self):
      return self.name +" "+ self.surname
 
-province = (
+class Farm(models.Model):
+    tenure = (
+   ('Off','Offer Letter'),
+   ('99','99-Year Lease'),
+   ('A1','A1 Permit'),
+   ('AP', 'A1 Temporal Permit'),
+   ('A2','A2 Permit'),
+   ('Ttl','Title Deed'),
+   ('DG','Deed of Grant'),
+   ('CCT','Certificate of Consolidated Title'),
+   )
+
+    water = (
+   ('D','Dam'),
+   ('R','River'),
+   ('B','Borehole'),
+   ('W', 'Weir'),
+   ('Wl','Well'),
+   ('N','None'),
+   )
+    province = (
    ('MTN','Matabeleland North'),
    ('MTS','Matabeleland South'),
    ('MSE','Mashonaland East'),
@@ -47,8 +50,7 @@ province = (
     ('MID','Midlands'),
     ('HRE','Harare'),
     ('BYO','Bulawayo'),
-)
-class Farm(models.Model):
+   )
     farmer = models.ForeignKey(Farmer,on_delete = models.CASCADE)
     farm_name = models.CharField(max_length=500)
     plot_number = models.CharField(max_length=250,null=True, blank=True)
@@ -57,7 +59,7 @@ class Farm(models.Model):
     province = models.CharField(max_length=250,choices=province)
     ward = models.CharField(max_length=10,null=True, blank=True)
     tenure_type = models.CharField(max_length=50,choices=tenure)
-    irrigation =models.CharField(max_length=250,null= True,default ="Do you have irrigation",choices=(('Y','Yes'),('N','No')))
+    irrigation =models.CharField(max_length=250,null= True,choices=(('Y','Yes'),('N','No')))
     water_availability  = models.CharField(max_length=250,choices=water)
     id = models.UUIDField(default=uuid.uuid1,unique=True,primary_key=True,editable=False)
 
